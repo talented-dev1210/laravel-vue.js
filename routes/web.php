b38c5bc78e10d1b30b0effb0382aa2b580e17c3c
talented-dev1210 <talenteddev1210@gmail.com>
2023-10-17 23:29:11 +0800

    Optimize the components

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,14 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', 'HomeController@index');
 
 Route::any('/spa/{any?}', 'SpaController@spa')->where(['any' => '.*']);
 
-Route::resource('project', 'ProjectController',['except' => ['create']]);
+Route::resource('project', 'ProjectController', ['except' => ['create']]);
 Route::get('projects/', 'ProjectController@projects');
 
-Route::resource('note', 'NoteController',['except' => ['create']]);
+Route::resource('note', 'NoteController', ['except' => ['create']]);
 Route::get('notes/', 'NoteController@notes');
 
 Route::post('contact', 'SpaController@addContact');
